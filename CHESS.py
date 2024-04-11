16c7228bc7d1a704c2cf23410d09167f2a026ab8
henry112q <138253381+henry112q@users.noreply.github.com>
2024-04-11 15:49:38 +0100

    castling

--- CHESS.py
+++ CHESS.py
@@ -21,7 +21,9 @@ def DisplayBoard(A8,B8,C8,D8,E8,F8,G8,H8,
 
 def StartValidation(turn,WKM,BKM,A1,B1,C1,D1,E1,F1,G1,H1,A8,B8,C8,D8,E8,F8,G8,H8,WR1M,WR2M,BR1M,BR2M):
     while True:
+        
         startSquare = input("Choose the coordinates of the piece you which to move: ").upper()
+        
         if startSquare in ("A1","A2","A3","A4","A5","A6","A7","A8",
                            "B1","B2","B3","B4","B5","B6","B7","B8",
                            "C1","C2","C3","C4","C5","C6","C7","C8",
@@ -31,49 +33,59 @@ def StartValidation(turn,WKM,BKM,A1,B1,C1,D1,E1,F1,G1,H1,A8,B8,C8,D8,E8,F8,G8,H8
                            "G1","G2","G3","G4","G5","G6","G7","G8",
                            "H1","H2","H3","H4","H5","H6","H7","H8",
                            "CK","CQ"):
+            
             if startSquare in ("CK","CQ"):
                 colour = turn % 2
+                
                 if colour == 0:
+                    
                     if startSquare == "CK":
                         if F1 == " " and G1 == " " and WKM == 0 and WR2M == 0:
                             return ("Castle","WK")
                         else:
                             print("Castle could not be completed due to one of two reasons \nF1 and G1 are not empty or King or Knight has already moved")
+                    
                     else:
                         if B1 == " " and C1 == " " and D1 == " " and WKM == 0 and WR1M == 0:
                             return ("Castle","WQ")
                         else:
                             print("Castle could not be completed due to one of two reasons \nB1 C1 and D1 are not empty or King or Knight has already moved")
+                
                 if colour == 1:
+                    
                     if startSquare == "CK":
                         if F8 == " " and G8 == " " and BKM == 0 and BR2M == 0:
                             return ("Castle","BK")
                         else:
                             print("Castle could not be completed due to one of two reasons \nF8 and G8 are not empty or King or Knight has already moved")
+                    
                     else:
                         if B8 == " " and C8 == " " and D8 == " " and BKM == 0 and BR1M == 0:
                             return ("Castle","BQ")
                         else:
                             print("Castle could not be completed due to one of two reasons \nB8 C8 and D8 are not empty or King or Knight has already moved")
             else:
+                
                 if globals()[startSquare] == " ":
                     print("No piece at this Coordnate")
                 else:
                     colour = turn%2
+                    
                     if colour == 0 and globals()[startSquare][0] == "W":
                         if globals()[startSquare]=="WK":
                             WKM += 1
-                        elif globals()[startSquare]=="WR1":
+                        elif startSquare=="A1":
                             WR1M += 1
-                        elif globals()[startSquare]=="WR2":
+                        elif startSquare == "H1":
                             WR2M += 1
                         break
+                    
                     elif colour == 1 and globals()[startSquare][0] == "B":
                         if globals()[startSquare]=="BK":
                             BKM += 1
-                        elif globals()[startSquare]=="BR1":
+                        elif startSquare=="A8":
                             BR1M += 1
-                        elif globals()[startSquare]=="BR2":
+                        elif startSquare=="H8":
                             BR2M += 1
                         break
                     else:
@@ -234,42 +246,50 @@ while True:
                  chessBoard)
     startsquare = StartValidation(turn,WKM,BKM,A1,B1,C1,D1,E1,F1,G1,H1,BLA,WR2,WK,WR1M,WR2M,BR1M,BR2M)
     if startsquare[0] != "Castle":
-        if globals()[startsquare[1]][-1] == "P":
+        
+        if globals()[startsquare][-1] == "P":
             typeOfPiece = "P"
-        elif globals()[startsquare[1]][-1] == "R":
+        elif globals()[startsquare][-1] == "R":
             typeOfPiece = "R"
-        elif globals()[startsquare[1]][-1] == "N":
+        elif globals()[startsquare][-1] == "N":
             typeOfPiece = "N"
-        elif globals()[startsquare[1]][-1] == "B":
+        elif globals()[startsquare][-1] == "B":
             typeOfPiece = "B"
-        elif globals()[startsquare[1]][-1] == "K":
+        elif globals()[startsquare][-1] == "K":
             typeOfPiece = "K"
-        elif globals()[startsquare[1]][-1] == "Q":
+        elif globals()[startsquare][-1] == "Q":
             typeOfPiece = "Q" 
+        
         endsquare = EndValidation()
         piece = globals()[startsquare]
         globals()[startsquare] = BLA
         globals()[endsquare] = piece
+        
     else:
-        castletype = startsquare[1] 
+        castletype = startsquare[1]
+         
         if castletype == "WK":
             G1 = WK
             F1 = WR2
             E1 = BLA
             H1 = BLA
+            
         elif castletype == "WQ": 
             B1 = WK
             C1 = WR1
             E1 = BLA
             A1 = BLA
+        
         elif castletype == "BK": 
             G8 = BK
             F8 = BR2
             E8 = BLA
             H8 = BLA
+        
         elif castletype == "BQ": 
             B8 = BK
             C8 = BR1
             E8 = BLA
             A8 = BLA
+    
     turn += 1
