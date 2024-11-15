c85c234f5ba8ca28b7c20d7517a029adf253f3f3
henry112q <138253381+henry112q@users.noreply.github.com>
2024-11-15 22:46:21 +0000

    Comments

--- CHESS.py
+++ CHESS.py
@@ -22,36 +22,46 @@ class Main:
         self.CHESSBOARD = " \n   ---A-----B-----C-----D-----E-----F-----G-----H---\n 8 |{:^5}|{:^5}|{:^5}|{:^5}|{:^5}|{:^5}|{:^5}|{:^5}| \n | -------------------------------------------------\n 7 |{:^5}|{:^5}|{:^5}|{:^5}|{:^5}|{:^5}|{:^5}|{:^5}| \n | -------------------------------------------------\n 6 |{:^5}|{:^5}|{:^5}|{:^5}|{:^5}|{:^5}|{:^5}|{:^5}| \n | -------------------------------------------------\n 5 |{:^5}|{:^5}|{:^5}|{:^5}|{:^5}|{:^5}|{:^5}|{:^5}| \n | -------------------------------------------------\n 4 |{:^5}|{:^5}|{:^5}|{:^5}|{:^5}|{:^5}|{:^5}|{:^5}| \n | -------------------------------------------------\n 3 |{:^5}|{:^5}|{:^5}|{:^5}|{:^5}|{:^5}|{:^5}|{:^5}| \n | -------------------------------------------------\n 2 |{:^5}|{:^5}|{:^5}|{:^5}|{:^5}|{:^5}|{:^5}|{:^5}| \n | ------------------------------------------------- \n 1 |{:^5}|{:^5}|{:^5}|{:^5}|{:^5}|{:^5}|{:^5}|{:^5}|\n   ------------------------------------------------- \n"
     
     def MoveInputs(self):
+        
         self.FirstMoveInvalid = True
+        
         while self.FirstMoveInvalid:
-            self.FirstMove = input("Enter the coordinate of the piece that you want to move: ")
-            if self.FirstMove.isalpha() == True:
-                if self.FirstMove.upper() in ("Q","QUIT"):
+            
+            self.moveInput = input("Enter the coordinate of the piece that you want to move: ")
+            
+            if self.moveInput.isalpha() == True and self.moveInput.upper() not in ("CQ","CK"): # Checks if the Input has a number and letter in it
+                
+                if self.moveInput.upper() in ("Q","QUIT"): # Checks if the user wants to end the game prematurely
                     self.FirstMoveInvalid = False
                     self.RUNNING = False
+                    continue
+                
+                print("The Input Must contain a number the Required format is Letter A-H then Number 1-8 or CQ/CK")
+                continue
                     
-            if len(self.FirstMove) != 2:
+            if len(self.moveInput) != 2:
+                print("Length of input is greater than two the Required format is Letter A-H then Number 1-8 or CQ/CK") # Checks for a Invalid Cooridnate
                 continue
             
-            if self.FirstMove.upper() in ("CK","CQ"):
+            if self.moveInput.upper() in ("CK","CQ"): # Checks for castling
                 self.CastlingEvent = True
                 self.FirstMoveInvalid = False if self.castlingValidation() else True
-                string = "Castling with the seleceted side "+self.FirstMove.upper() if self.FirstMoveInvalid == False else ""
+                string = "Castling with the seleceted side "+self.moveInput.upper() if self.FirstMoveInvalid == False else ""
                 
             
-            if self.FirstMove[0].upper() not in ["A","B","C","D","E","F","G","H","C"] or self.FirstMove[1] not in ["1","2","3","4","5","6","7","8","K","Q"]:
+            if self.moveInput[0].upper() not in ["A","B","C","D","E","F","G","H","C"] or self.moveInput[1] not in ["1","2","3","4","5","6","7","8","K","Q"]:
                 continue
             
-            if self.FirstMove.upper() not in ("CK","CQ"):
+            if self.moveInput.upper() not in ("CK","CQ"):
             
-                self.AlphaLocation = ord(self.FirstMove[0].upper())-65
-                self.NumericalLocation = int(self.FirstMove[1])-1
+                self.AlphaLocation = ord(self.moveInput[0].upper())-65
+                self.NumericalLocation = int(self.moveInput[1])-1
                 self.FirstPiece = self.PieceArray[self.NumericalLocation][self.AlphaLocation]
             
                 if (self.FirstPiece[0] == "W" and self.Turn == True) or (self.FirstPiece[0] == "B" and self.Turn == False) or self.FirstPiece == "  ":
                     continue
                 
-                string = self.FirstPiece+" at postion "+self.FirstMove
+                string = self.FirstPiece+" at postion "+self.moveInput
             
             confirm = input("The piece you have selected is {} enter Y if correct and N if incorrect : ".format(string))
             if not confirm.isalpha() or len(confirm) != 1 :
@@ -66,14 +76,14 @@ class Main:
         
         Validate = False
         
-        if self.FirstMove.upper() == "CK" :
+        if self.moveInput.upper() == "CK" :
             if self.WhiteKingMove == False and self.WhiteRRMove == False and self.Turn == False :
                 Validate = True
                 
             if self.WhiteKingMove == False and self.WhiteRRMove == False and self.Turn == True :
                 Validate = True
                 
-        if self.FirstMove.upper() == "CQ" :
+        if self.moveInput.upper() == "CQ" :
             if self.WhiteKingMove == False and self.WhiteLRMove == False and self.Turn == False :
                 Validate = True
                 
